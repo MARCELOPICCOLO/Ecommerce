27ca765a2afb72d489a2ec4c00b9b8f0a7f51b94
MARCELOPICCOLO <marcelociabatari@hotmail.com>
2021-01-22 18:22:42 -0300

    crud upadate e deete da tabela Products

--- src/controller/productContoller.ts
+++ src/controller/productContoller.ts
@@ -1,6 +1,6 @@
 import {getRepository} from 'typeorm';
 import {Product} from '../entity/Product';
-import {Request, Response} from 'express';
+import {request, Request, response, Response} from 'express';
 import {uuid} from 'uuidv4';
 
 
@@ -15,5 +15,27 @@ export const Create = async (request: Request, response : Response)=>{
    response.json(product); 
 }
 
+export const Update = async (request: Request, response: Response)=>{
+    const {id} = request.params;
+    const product = await getRepository(Product).update(id, request.body);
+
+    if(product.affected===1){
+        const productUpdated = await getRepository(Product).findOne(id);
+        return response.json(productUpdated);
+    }
+    return response.status(404).json('product not found');
+}
+
+export const Delete = async (request: Request, response: Response)=>{
+    const {id} = request.params;
+    const product = await getRepository(Product).delete(id);
+
+    if(product.affected===1){
+        const res = await getRepository(Product).findOne(id);
+        return response.json('Product removed'); 
+    }
+
+    return response.status(404).json('product not found');
+}
 
 
--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,9 +1,11 @@
 import {Router}from 'express';
-import {List,Create} from '../controller/productContoller';
+import {List,Create, Update, Delete} from '../controller/productContoller';
 
 const router = Router();
 
 router.get('/list', List);
 router.post('/create',Create);
+router.put('/update/:id', Update);
+router.put('/delete/:id',Delete);
 
 export default router; 
