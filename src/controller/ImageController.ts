495c440f548262d8f65b451a324fd35211e88cab
MARCELOPICCOLO <marcelociabatari@hotmail.com>
2021-02-02 22:42:38 -0300

    integração da google api

--- src/controller/ImageController.ts
+++ src/controller/ImageController.ts
@@ -3,14 +3,15 @@ import {getRepository}from  'typeorm'
 import {Image} from  '../entity/Image'
 
 
+
 export const getImage = async (request,response)=>{
       const list = await getRepository(Image).find();
       return response.json(list);
 }
 
 
-export const createImage = async (request,reponse)=>{
-      const image = await getRepository(Image).save(request.body);
-      return response.json(image);
+export const createImage = async (request,reponse)=>{   
+    
+      return response.json({message: 'imagem carregada'});
 }
 
--- src/index.ts
+++ src/index.ts
@@ -4,14 +4,13 @@ import router from  './routes/routes';
 import { createConnection } from "typeorm";
 import * as cors from 'cors';
 
+
 createConnection()
 
 const app = express();
 app.use(cors());
 app.use(express.json());
-app.use(router);
 
-
-app.listen(3001,()=>{
+app.listen(3333,()=>{
     console.log('its running');
-})
+});
